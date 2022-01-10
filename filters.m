f3ff45b3fe59627cb0d43ba6fe422fdc0c42ec2b
Rebecca McCabe <rmccabe@mit.edu>
2022-01-10 03:06:21 -0500

    fit the filtrete PQ curve

--- filters.m
+++ filters.m
@@ -1,6 +1,18 @@
-% Filtrete 3M
+% Filtrete 3M mpp300
 
+flow = [290 430 575 720 970]; % CFM; 
+pressure = [.1  .17 .24 .34 .51]*249.09; % inches water column converted to Pa
 
-% CFM; inches water column converter to Pa
-mpp300 = [290 430 575 720 970;
- [.1  .17 .24 .34 .51]*249.09];
+f = [0 13.5 flow];
+p = [0 2.5 pressure];
+
+figure
+plot(f,p)
+title('3M Filtrete mpp300')
+
+k = 15e-5;
+p_fit = k*f.^2;
+
+hold on
+plot(f,p_fit)
+legend('actual','fit')
